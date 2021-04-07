69f11320756491d18f4703172f2ce749d20470f8
tesdeve <tesdeve@gmail.com>
2021-04-07 08:32:30 +0100

    Adds some styling to index.html.erb

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  # Annotate adds comments summarizing a specific model's schema.
+  gem 'annotate'
 end
 
 group :development do
@@ -39,7 +41,7 @@ group :development do
   gem 'rack-mini-profiler', '~> 2.0'
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
+  gem 'spring' 
 end
 
 group :test do
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id               :bigint           not null, primary key
+#  action           :string
+#  aduana           :decimal(, )
+#  bought           :boolean
+#  exchange_rate    :float
+#  flete            :decimal(, )
+#  impuesto         :decimal(, )
+#  name             :string
+#  peso             :float
+#  seguro           :decimal(, )
+#  total_cost       :decimal(, )
+#  total_envio_cop  :decimal(, )
+#  total_envio_usd  :decimal(, )
+#  valor_cop        :decimal(, )
+#  valor_maximo_cop :decimal(, )
+#  valor_usd        :decimal(, )
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
