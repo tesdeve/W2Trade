c1388a1b2d40ab9b100acf4a161af2d00985c4bd
tesdeve <tesdeve@gmail.com>
2021-04-07 10:24:56 +0100

    Adds service-workers

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,20 +1,41 @@
 //const { environment } = require('@rails/webpacker') Commented out to favor PWA
-//const { resolve } = require('path');
-//const { config, environment, Environment } = require('@rails/webpacker');
-//
-//const WebpackerPwa = require('webpacker-pwa');
-//new WebpackerPwa(config, environment);
+const { resolve } = require('path');
+const { config, environment, Environment } = require('@rails/webpacker');
 
-const { environment } = require('@rails/webpacker')
+const WebpackerPwa = require('webpacker-pwa');
+new WebpackerPwa(config, environment);
 
 const webpack = require('webpack')
 
 environment.plugins.prepend('Provide', 
 	new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']
+    jQuery: 'jquery/src/jquery'
   })
 )
 
 module.exports = environment
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////
+
+// const { environment } = require('@rails/webpacker')
+
+// const webpack = require('webpack')
+
+// environment.plugins.prepend('Provide', 
+// 	new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery',
+//     jQuery: 'jquery/src/jquery',
+//     Popper: ['popper.js', 'default']
+//   })
+// )
+
+// module.exports = environment
